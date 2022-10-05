efb75cc883833648c40d3725575ee4581bc7cc47
mubakid <bakiddev@gmail.com>
2022-10-06 00:10:23 +0700

    add factory for student

--- database/factories/StudentFactory.php
+++ database/factories/StudentFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +18,26 @@ class StudentFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'nama' => $this->faker->name(),
+            'user_id' => User::factory(),
+            'nik' => $this->faker->word(),
+            'nis' => rand(222222, 999999),
+            'tanggal_lahir' => $this->faker->word(),
+            'tempat_lahir' => $this->faker->word(),
+            'jenis_kelamin' => $this->faker->randomElement(['L', 'P']),
+
+            // alamat rumah
+            'alamat' => $this->faker->name(),
+            'rtrw' => $this->faker->name(),
+            'desa' => $this->faker->streetName(),
+            'kota' => $this->faker->city(),
+            'kecamatan' => $this->faker->city(),
+            'provinsi' => $this->faker->city(),
+            'kode_pos' => $this->faker->name(),
+            'madin_education_id' => rand(1, 9),
+            'formal_education_id' => rand(1, 9),
+            'dormitory_id' => rand(1, 9),
+            'room' => rand(1, 9),
         ];
     }
 }

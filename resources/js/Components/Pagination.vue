ac37d92ab0627c1c490e24b54fa8b3ad264e6915
mubakid <bakiddev@gmail.com>
2022-10-07 04:26:31 +0700

    fixed pagination and debounce with loading animation spinner bootstrap

---
+++ resources/js/Components/Pagination.vue
@@ -0,0 +1,22 @@
+<template>
+    <tr>
+        <div class="d-md-flex justify-content-end">
+            <template v-for="(link, key) in links" :key="key">
+                <div class="" v-if="link.url === null"></div>
+                <Link
+                    v-else
+                    :href="link.url"
+                    v-html="link.label"
+                    class="btn btn-sm btn-outline-secondary mx-1 my-1"
+                    :class="{ 'btn-primary': link.active }"
+                ></Link>
+            </template>
+        </div>
+    </tr>
+</template>
+<script setup>
+import { defineProps } from "vue";
+defineProps({
+    links: Object,
+});
+</script>
---
+++ resources/js/Components/SimplePagination.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="d-flex justify-content-between">
+        <div>
+            <Link
+                as="button"
+                class="btn btn-sm btn-outline-secondary mx-1 my-1"
+                :href="links.first_page_url"
+                >First</Link
+            >
+            <Link
+                as="button"
+                class="btn btn-sm btn-outline-secondary mx-1 my-1"
+                :href="links.prev_page_url"
+                >&laquo
+            </Link>
+
+            <Link
+                as="button"
+                class="btn btn-sm btn-primary mx-1 my-1"
+                :href="links.current_page"
+                v-html="links.current_page"
+                disabled
+            ></Link>
+
+            <Link
+                as="button"
+                class="btn btn-sm btn-outline-secondary mx-1 my-1"
+                :href="links.next_page_url"
+                >&raquo
+            </Link>
+            <Link
+                as="button"
+                class="btn btn-sm btn-outline-secondary mx-1 my-1"
+                :href="links.last_page_url"
+                >Last</Link
+            >
+        </div>
+        <div>
+            <div class="btn btn-sm btn-secondary my-1 mx-1">
+                Total : {{ links.total }}
+            </div>
+        </div>
+    </div>
+</template>
+<script setup>
+import { defineProps } from "vue";
+defineProps({
+    links: Object,
+});
+</script>
